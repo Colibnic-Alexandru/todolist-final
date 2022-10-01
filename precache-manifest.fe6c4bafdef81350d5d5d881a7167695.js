bc9f07ac92bde35296f533607120986bd91d6504
Colibnic-Alexandru <mr.kolibnik@gmail.com>
2022-10-01 16:49:26 +0300

    Updates

---
+++ precache-manifest.fe6c4bafdef81350d5d5d881a7167695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f636e1230d5fbd3c75c5bfb39db898",
+    "url": "/todolist-final/index.html"
+  },
+  {
+    "revision": "bd7ac63037ecb56f9887",
+    "url": "/todolist-final/static/css/main.43730ec5.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todolist-final/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-final/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7ac63037ecb56f9887",
+    "url": "/todolist-final/static/js/main.fd1bd2b1.chunk.js"
+  },
+  {
+    "revision": "133d63f3069b34a9d0f0",
+    "url": "/todolist-final/static/js/runtime-main.263429b5.js"
+  }
+]);
